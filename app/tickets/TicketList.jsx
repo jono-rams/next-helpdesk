d98de16b387dbc67a7ab5bcf2a17f8b4c0b5cde4
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-10 18:10:51 -0400

    Dynamic Segments

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 async function getTickets() {
   const res = await fetch("http://localhost:4000/tickets", {
     next: {
-      revalidate: 0 // use 0 to opt out of using cache
-    }
+      revalidate: 0, // use 0 to opt out of using cache
+    },
   });
-  
+
   return res.json();
 }
 
@@ -13,16 +15,20 @@ export default async function TicketList() {
 
   return (
     <>
-      {tickets.length > 0 && tickets.map((ticket) => (
+      {tickets.map((ticket) => (
         <div key={ticket.id} className="my-5 card">
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}</p>
-          <div className={`pill ${ticket.priority}`}>{ticket.priority} priority</div>
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
         </div>
       ))}
       {tickets.length === 0 && (
         <p className="text-center">There are no open tickets, yay!</p>
       )}
     </>
-  )
+  );
 }
