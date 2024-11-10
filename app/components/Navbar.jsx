e721c522f42af2506fcf366489ca3601c8d0b0ef
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-10 14:24:38 -0400

    Styles, Fonts & Images

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,10 +1,22 @@
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "./dojo-logo.png";
+
 export default function Navbar() {
   return (
     <nav>
-      <h1>
-        <Link href="/">Jono's Helpdesk</Link>
-      </h1>
+      <Link href="/">
+        <Image
+          src={Logo}
+          alt="Dojo Helpdesk logo"
+          width={70}
+          quality={100}
+          placeholder="blur"
+          priority="true"
+          className="inline-block mr-2"
+        />
+        <h1 className="inline-block">Jono's Helpdesk</h1>
+      </Link>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
     </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        primary: '#7856ff',
+        secondary: '#858dfb',
+        tertiary: '#b2c0fb',
+      }
     },
   },
   plugins: [],
