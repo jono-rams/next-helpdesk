099f87ecbad6c235490d279b77ecada2b63617c1
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-11 11:12:12 -0400

    Signing up a new user

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,18 +1,40 @@
-"use client"
+'use client';
 
-import AuthForm from "../AuthForm";
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+import AuthForm from '../AuthForm';
 
 export default function Signup() {
+  const router = useRouter();
+  const [error, setError] = useState('');
+
   const handleSubmit = async (e, email, password) => {
     e.preventDefault();
-    console.log('user signup', email, password);
-  }
+
+    const supabase = createClientComponentClient();
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/api/auth/callback`,
+      },
+    });
+
+    if (error) {
+      setError(error.message);
+    } else {
+      router.push('/verify');
+    }
+  };
 
   return (
     <main>
       <h2 className="text-center">Sign Up</h2>
 
       <AuthForm handleSubmit={handleSubmit} />
+      {error && <div className="error">{error}</div>}
     </main>
-  )
+  );
 }
---
+++ app/(auth)/verify/page.jsx
@@ -0,0 +1,8 @@
+export default function Verify() {
+  return (
+    <main className="text-center">
+      <h2>Thanks for registering!</h2>
+      <p>Before accessing the site, please check your email for a verification link.</p>
+    </main>
+  )
+}
