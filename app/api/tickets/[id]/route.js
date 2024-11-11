ef74b8224efa7d0cf709026c7e370e2ff6a3baa5
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-10 21:46:31 -0400

    Route Handler Params

---
+++ app/api/tickets/[id]/route.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(_, { params }) {
+  const { id } = params;
+
+  const res = await fetch(`http://localhost:4000/tickets/${id}`);
+  
+  if (!res.ok) {
+    return NextResponse.json({ error: "Ticket not found" }, { status: 404 });
+  }
+  
+  const ticket = await res.json();
+
+  return NextResponse.json(ticket, { status: 200 });
+}
