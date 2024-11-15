4a503155f15212e1d92b6a8050b77612b62577e9
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-15 08:14:02 -0400

    Using useFormStatus Hook

--- app/(dashboard)/tickets/create/CreateForm.jsx
+++ app/(dashboard)/tickets/create/CreateForm.jsx
@@ -1,10 +1,7 @@
-"use client";
-
-import { useState } from "react";
+import SubmitButton from "@/app/components/SubmitButton";
 import { addTicket } from "../actions";
 
 export default function CreateForm() {
-  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <form className="w-1/2" action={addTicket}>
@@ -33,10 +30,7 @@ export default function CreateForm() {
           <option value="high">High Priority</option>
         </select>
       </label>
-      <button className="btn-primary" disabled={isLoading}>
-        {isLoading && <span>Adding...</span>}
-        {!isLoading && <span>Add Ticket</span>}
-      </button>
+      <SubmitButton submitText="Add Ticket" pendingText="Adding..." />
     </form>
   );
 }
