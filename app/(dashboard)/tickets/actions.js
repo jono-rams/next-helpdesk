8bd99478ee49ceb064a5b84c39b71333b467b06f
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-15 08:07:50 -0400

    Handling a POST Request in a Server Action

--- app/(dashboard)/tickets/actions.js
+++ app/(dashboard)/tickets/actions.js
@@ -1,5 +1,28 @@
 "use server"
 
-export async function addTicket() {
-  console.log("Hello from the server action!")
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function addTicket(formData) {
+  const ticket = Object.fromEntries(formData);
+
+  const supabase = createServerActionClient({cookies});
+
+  const { data: {session}} = await supabase.auth.getSession();
+
+  // insert the data
+  const { error } = await supabase.from("tickets")
+    .insert({
+      ...ticket,
+      user_email: session.user.email
+    })
+
+  if (error) {
+    console.error(error.message);
+  }
+
+  revalidatePath("/tickets");
+  redirect("/tickets");
 }
--- app/(dashboard)/tickets/create/CreateForm.jsx
+++ app/(dashboard)/tickets/create/CreateForm.jsx
@@ -1,42 +1,10 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { addTicket } from "../actions";
 
 export default function CreateForm() {
-  const router = useRouter();
-
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-  const [priority, setPriority] = useState("low");
   const [isLoading, setIsLoading] = useState(false);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-
-    const ticket = { title, body, priority };
-
-    const res = await fetch("http://localhost:3000/api/tickets", {
-      method: "POST",
-      body: JSON.stringify(ticket),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    
-    const json = await res.json();
-
-    if (json.error) {
-      console.error(json.error.message);
-    }
-    else if (json.data) {
-      router.refresh();
-      router.push("/tickets");
-    }
-    
-  };
 
   return (
     <form className="w-1/2" action={addTicket}>
@@ -44,22 +12,22 @@ export default function CreateForm() {
         <span>Title:</span>
         <input
           required
+          name="title"
           type="text"
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
         />
       </label>
       <label>
         <span>Body:</span>
         <textarea
           required
-          onChange={(e) => setBody(e.target.value)}
-          value={body}
+          name="body"
         />
       </label>
       <label>
         <span>Priority:</span>
-        <select onChange={(e) => setPriority(e.target.value)} value={priority}>
+        <select
+          name="priority"
+        >
           <option value="low">Low Priority</option>
           <option value="medium">Medium Priority</option>
           <option value="high">High Priority</option>
